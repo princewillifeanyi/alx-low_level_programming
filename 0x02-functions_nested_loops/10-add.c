40389e865814688ce9d5edea2fddd78f1dfb9a61
princewill ifeanyi <princewillebube315@gmail.com>
2022-09-13 16:42:05 -0700

    i sabi this one

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @num: Caracters to add
+ * @num1: second character
+ *
+ * Return: Always return the added number
+ */
+int add(int num, int num1)
+{
+	return (num + num1);
+}
