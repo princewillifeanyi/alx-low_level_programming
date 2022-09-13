097d0e4359ccf9126d4837c697f7cb566c621f4c
princewill ifeanyi <princewillebube315@gmail.com>
2022-09-13 16:14:46 -0700

    why me God

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,15 @@
 #include "main.h"
-
-/*
+/**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- *
- * return always 0
  */
 
 void print_alphabet(void)
 {
-	char lowercase = 'a';
+	char c = 'a';
 
-	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(lowercase);
+		_putchar(c);
 	}
 	_putchar('\n');
-
 }
