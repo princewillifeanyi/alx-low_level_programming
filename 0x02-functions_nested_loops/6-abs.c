567006a54bffca95b5df2ee23695d4fcd153c3ce
princewill ifeanyi <princewillebube315@gmail.com>
2022-09-13 16:30:15 -0700

    its not giving abeg

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Compute absolute value.
+ *@n: Character to convert
+ *
+ * Return: returns absolute value of integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
