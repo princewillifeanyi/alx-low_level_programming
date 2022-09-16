1d385b882badebc432d0e0f2bfaacbbfa0a103cf
princewill ifeanyi <princewillebube315@gmail.com>
2022-09-16 11:19:39 -0700

    Miserere

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main- prints either fizz, buzz or fizzbuzz
+ *
+ * Return: Always o
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if ((num % 3) == 0)
+			printf("Fizz");
+		else if ((num % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
